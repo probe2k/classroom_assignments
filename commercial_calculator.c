750ab1a86b0d771e6bfafebe5ed3bc83adbdca4b
probe2k <kyash054@gmail.com>
2020-06-16 15:33:55 +0530

    Switch to LLVM Formatter

--- commercial_calculator.c
+++ commercial_calculator.c
@@ -1,46 +1,47 @@
-#include<stdio.h>
-#include<math.h>
+#include <math.h>
+#include <stdio.h>
 
 void main() {
-    float op1, op2;
-    int choice;
+  float op1, op2;
+  int choice;
 
-    printf("COMMERCIAL CALCULATOR");
-    printf("\n1. SUM\n2. DIFFERENCE\n3. PRODUCT\n4. QUOTIENT\n5. SQUARE ROOT\n6. POWER");
-    scanf("%d", &choice);
+  printf("COMMERCIAL CALCULATOR");
+  printf("\n1. SUM\n2. DIFFERENCE\n3. PRODUCT\n4. QUOTIENT\n5. SQUARE ROOT\n6. "
+         "POWER");
+  scanf("%d", &choice);
 
-    switch (choice) {
-        case 1:
-            printf("\nEnter the operands :");
-            scanf("%f%f", &op1, &op2);
-            printf("Sum is : %f", (op1 + op2));
-            break;
-        case 2:
-            printf("\nEnter operands :");
-            scanf("%f%f", &op1, &op2);
-            printf("Difference is : %f", (op1 - op2));
-            break;
-        case 3:
-            printf("\nEnter operands :");
-            scanf("%f%f", &op1, &op2);
-            printf("Product is : %f", (op1 * op2));
-            break;
-        case 4:
-            printf("\nEnter operands :");
-            scanf("%f%f", &op1, &op2);
-            printf("Quotient is : %f", (op1 / op2));
-            break;
-        case 5:
-            printf("\nEnter operand :");
-            scanf("%f", &op1);
-            printf("Square Root is : %f", (sqrt((int)op1)));
-            break;
-        case 6:
-            printf("\nEnter operands :");
-            scanf("%f%f", &op1, &op2);
-            printf("%f raised to the pwoer %f is : %f", op1, op2, pow(op1, op2));
-            break;
-        default :
-            printf("Wrong choice!");
-    }
+  switch (choice) {
+  case 1:
+    printf("\nEnter the operands :");
+    scanf("%f%f", &op1, &op2);
+    printf("Sum is : %f", (op1 + op2));
+    break;
+  case 2:
+    printf("\nEnter operands :");
+    scanf("%f%f", &op1, &op2);
+    printf("Difference is : %f", (op1 - op2));
+    break;
+  case 3:
+    printf("\nEnter operands :");
+    scanf("%f%f", &op1, &op2);
+    printf("Product is : %f", (op1 * op2));
+    break;
+  case 4:
+    printf("\nEnter operands :");
+    scanf("%f%f", &op1, &op2);
+    printf("Quotient is : %f", (op1 / op2));
+    break;
+  case 5:
+    printf("\nEnter operand :");
+    scanf("%f", &op1);
+    printf("Square Root is : %f", (sqrt((int)op1)));
+    break;
+  case 6:
+    printf("\nEnter operands :");
+    scanf("%f%f", &op1, &op2);
+    printf("%f raised to the pwoer %f is : %f", op1, op2, pow(op1, op2));
+    break;
+  default:
+    printf("Wrong choice!");
+  }
 }
--- quadratic_equation.c
+++ quadratic_equation.c
@@ -1,30 +1,25 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
-int main()
-{
-    double a, b, c, D, r1, r2, realPart, imagPart;
+int main() {
+  double a, b, c, D, r1, r2, realPart, imagPart;
 
-    printf("\nEnter coefficients a, b, and c: ");
-    scanf("%lf%lf%lf", &a, &b, &c);
-    D = b * b - 4 * a * c;
+  printf("\nEnter coefficients a, b, and c: ");
+  scanf("%lf%lf%lf", &a, &b, &c);
+  D = b * b - 4 * a * c;
 
-    if (D > 0)
-    {
-        r1 = (-b + sqrt(D)) / (2 * a);
-        r2 = (-b - sqrt(D)) / (2 * a);
-        printf("Root 1 = %.2lf & Root 2 = %.2lf", r1, r2);
-    }
-    else if (D == 0)
-    {
-        r1 = r2 = -b / (2 * a);
-        printf("Root 1 = %.2lf & Root 2 = %.2lf", r1, r2);
-    }
-    else
-    {
-        realPart = -b / (2 * a);
-        imagPart = sqrt(-D) / (2 * a);
-        printf("Root 1 = %.2lf+%.2lfi and Root 2 = %.2f-%.2fi", realPart, imagPart, realPart, imagPart);
-    }
-    return 0;
+  if (D > 0) {
+    r1 = (-b + sqrt(D)) / (2 * a);
+    r2 = (-b - sqrt(D)) / (2 * a);
+    printf("Root 1 = %.2lf & Root 2 = %.2lf", r1, r2);
+  } else if (D == 0) {
+    r1 = r2 = -b / (2 * a);
+    printf("Root 1 = %.2lf & Root 2 = %.2lf", r1, r2);
+  } else {
+    realPart = -b / (2 * a);
+    imagPart = sqrt(-D) / (2 * a);
+    printf("Root 1 = %.2lf+%.2lfi and Root 2 = %.2f-%.2fi", realPart, imagPart,
+           realPart, imagPart);
+  }
+  return 0;
 }
